28394dc07d1ccd3576a683f33c95347b3a99e485
Peter Karman <karpet@peknet.com>
2017-02-10 22:31:50 -0600

    real splash content

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations',  }
-  post '/signup' => 'splash#signup', as: :splash_signup
   get '/splash' => 'splash#index'
   root 'splash#index'
-  get '/setup' => 'setup#index'
 
-  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :alerts
 end
