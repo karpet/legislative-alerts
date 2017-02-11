557fa5e0b99bece85c6b14315d4d923fe05119fe
Peter Karman <karpet@peknet.com>
2017-02-10 23:53:33 -0600

    squash migrations

--- db/migrate/20170209042136_add_secret_token_to_identity.rb
+++
@@ -1,5 +0,0 @@
-class AddSecretTokenToIdentity < ActiveRecord::Migration
-  def change
-    add_column :identities, :secrettoken, :string
-  end
-end
--- db/migrate/20170209051606_add_refresh_token_to_identity.rb
+++
@@ -1,5 +0,0 @@
-class AddRefreshTokenToIdentity < ActiveRecord::Migration
-  def change
-    add_column :identities, :refreshtoken, :string
-  end
-end
