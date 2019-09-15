f96ddc1013cdab8fbd61e713094240b60344989f
Peter Karman <karpet@peknet.com>
2019-09-15 15:50:17 -0500

    syntax fixes

--- spec/factories/identities.rb
+++ spec/factories/identities.rb
@@ -1,14 +1,4 @@
 FactoryBot.define do
   factory :identity do
-    user nil
-    provider "MyString"
-    accesstoken "MyString"
-    uid "MyString"
-    name "MyString"
-    email "MyString"
-    nickname "MyString"
-    image "MyString"
-    phone "MyString"
-    urls "MyString"
   end
 end
