7f90356010be5ae580b893585a17befcb62c6c68
Peter Karman <karpet@peknet.com>
2019-09-15 23:01:25 -0500

    squash migrations

--- db/migrate/20170215023800_add_alert_columns.rb
+++
@@ -1,5 +0,0 @@
-class AddAlertColumns < ActiveRecord::Migration[5.0]
-  def change
-    add_column :alerts, :last_sent_at, :datetime
-  end
-end
--- db/migrate/20170221032627_drop_identity_cols.rb
+++
@@ -1,8 +0,0 @@
-class DropIdentityCols < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :identities, :name
-    remove_column :identities, :nickname
-    remove_column :identities, :phone
-    remove_column :identities, :urls
-  end
-end
